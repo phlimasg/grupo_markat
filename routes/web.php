0af68704215e4f74db0913e4470a21bf133b68e4
phlimasg <raphaelpc_@hotmail.com>
2019-03-11 23:51:56 -0300

    test sub domain
    test sub domain

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::domain('telhasimperio.grupomarkat.com')->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+});
+
