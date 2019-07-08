a7d0791c706acace8c9b5f75827587b1dc82cdee
phlimasg <raphaelpc_@hotmail.com>
2019-07-08 20:57:14 -0300

    Acerto do header

--- resources/views/imperio/index.blade.php
+++ resources/views/imperio/index.blade.php
@@ -8,14 +8,14 @@
 <div class="container">
     <div class="row fullscreen d-flex align-items-center justify-content-center">
         <div class="banner-content col-lg-12 col-md-12">
-            <h6 class="text-uppercase">Don’t look further, here is the key</h6>
+            <h6 class="text-uppercase">As melhores telhas, você encontra aqui!</h6>
             <h1>
-                We’re Industrial solution			
+                Nós somos a solução!			
             </h1>
             <p class="text-white">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et <br> dolore magna aliqua. Ut enim ad minim.  sed do eiusmod tempor incididunt.
+                Especializados em telhas galvalumes, temos o melhor material termoacústico do mercado.
             </p>
-            <a href="#" class="primary-btn header-btn text-uppercase">Get Started</a>
+            <a href="#" class="primary-btn header-btn text-uppercase">Faça seu orçamento</a>
         </div>												
     </div>
 </div>
