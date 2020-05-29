18894ac2ba8b07e3b21e998f573e0a940472057f
phlimasg <42648475+phlimasg@users.noreply.github.com>
2020-05-29 16:10:53 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,28 +16,25 @@
 });
 Route::domain('telhasimperio.grupomarkat.com')->group(function () {*/
 
-    Route::group(['scheme' => 'https'], function () {
-        
-        Route::get('/', 'Imperio\SiteController@index')->name('impoerioHome');
-        Route::get('/cadastro', 'Imperio\SiteController@cadastro')->name('cadastro');
-        Route::get('/produtos', 'Imperio\SiteController@produtos')->name('produtos');
-        Route::post('/carrinho', 'Imperio\SiteController@carrinhoStore')->name('carrinhoStore');
-        Route::get('/carrinho/{id}', 'Imperio\SiteController@carrinho')->name('carrinho');
-        Route::post('/carrinhoUpdate', 'Imperio\SiteController@carrinhoUpdate')->name('carrinhoUpdate');
-        Route::post('/carrinhoDelete', 'Imperio\SiteController@carrinhoDelete')->name('carrinhoDelete');
-        Route::post('/carrinhoFinalizar', 'Imperio\SiteController@carrinhoFinalizar')->name('carrinhoFinalizar');
-        Route::get('/concluido', function(){
-            return view('imperio.carrinhoFinal');
-        })->name('carrinhoFinal');
-        Route::get('/produto/{id}', 'Imperio\SiteController@show')->name('show');
-        Route::post('/salvar_cadastro', 'Imperio\SiteController@store')->name('cadastro.store');
-        Route::prefix('admin')->group(function () {
-            Route::resource('produto_imagens','Imperio\ImagensController');
-            Route::resource('produtos','Imperio\ProdutosController');    
-        });
-        
-    });
 
+
+Route::get('/', 'Imperio\SiteController@index')->name('impoerioHome');
+Route::get('/cadastro', 'Imperio\SiteController@cadastro')->name('cadastro');
+Route::get('/produtos', 'Imperio\SiteController@produtos')->name('produtos');
+Route::post('/carrinho', 'Imperio\SiteController@carrinhoStore')->name('carrinhoStore');
+Route::get('/carrinho/{id}', 'Imperio\SiteController@carrinho')->name('carrinho');
+Route::post('/carrinhoUpdate', 'Imperio\SiteController@carrinhoUpdate')->name('carrinhoUpdate');
+Route::post('/carrinhoDelete', 'Imperio\SiteController@carrinhoDelete')->name('carrinhoDelete');
+Route::post('/carrinhoFinalizar', 'Imperio\SiteController@carrinhoFinalizar')->name('carrinhoFinalizar');
+Route::get('/concluido', function(){
+    return view('imperio.carrinhoFinal');
+})->name('carrinhoFinal');
+Route::get('/produto/{id}', 'Imperio\SiteController@show')->name('show');
+Route::post('/salvar_cadastro', 'Imperio\SiteController@store')->name('cadastro.store');
+Route::prefix('admin')->group(function () {
+    Route::resource('produto_imagens','Imperio\ImagensController');
+    Route::resource('produtos','Imperio\ProdutosController');    
+});
 
 
 Auth::routes();
