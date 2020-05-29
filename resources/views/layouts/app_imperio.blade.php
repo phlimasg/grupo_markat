c8fd6d93373c8ce5ecc2646cff38714c3ae4eaef
phlimasg <42648475+phlimasg@users.noreply.github.com>
2020-05-29 19:41:43 -0300

    Update app_imperio.blade.php

--- resources/views/layouts/app_imperio.blade.php
+++ resources/views/layouts/app_imperio.blade.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="zxx" class="no-js">
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5HGDQZX');</script>
+    <!-- End Google Tag Manager -->
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168029134-1"></script>
     <script>
@@ -44,6 +51,10 @@
         <script src="{{ url('public/js/vendor/bootstrap.min.js')}}"></script>	
     </head>
     <body id="mypage">
+      <!-- Google Tag Manager (noscript) -->
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5HGDQZX"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
             <header id="header" id="home">
                     <div class="container main-menu">
                         <div class="row align-items-center justify-content-between d-flex">
